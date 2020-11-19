5035222d1ad9afcc3f7798e7e6fef28ebe172503
Iftesum-Zami <tmizami20@gmail.com>
2020-11-20 05:33:36 +0600

    2nd commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Header from './Components/Header/Header';
+import WhatWeDo from './Components/WhatWeDo/WhatWeDo';
+import StayRunning from './Components/StayRunning/StayRunning';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Header />
-      
+      <WhatWeDo />
+      <StayRunning />
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,11 +5,14 @@ const Header = () => {
     return (
         <div className="container">
             <div className="row">
-                <div className="col-md-6">
-                    <h1>Florence agency</h1>
-                    <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                    <button>See Pricing</button>
+                <div className="col-md-4 d-flex align-items-center">
+                    <div>
+                        <h1 className="pb-3">Florence agency</h1>
+                        <p className="pb-3">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        <button>See Pricing</button>
+                    </div>
                 </div>
+                <div className="col-md-2"></div>
                 <div className="col-md-6">
                     <img src={headerImg} className="img-fluid" alt=""/>
                 </div>
