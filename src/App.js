70de36e04490d86a2730bab7aa7ff1c0561ad300
MrRiaz <monsurrahmanriaz@gmail.com>
2020-11-20 18:05:37 +0600

    font-family update

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Header from './Components/Header/Header';
+import DedicatedTeam from './Components/DedicatedTeam/DedicatedTeam';
+import GetInTouch from './Components/GetInTouch/GetInTouch';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Header />
-      
+      <DedicatedTeam />
+      <GetInTouch />
+      <Footer />
     </div>
   );
 }
--- src/Components/GetInTouch/GetInTouch.js
+++ src/Components/GetInTouch/GetInTouch.js
@@ -6,9 +6,9 @@ const GetInTouch = () => {
         <section className="section">
             <div className="container">
                 <div className="row justify-content-center">
-                    <div className="col-md-7 text-right getInTouch">
+                    <div className="col-md-7 row getInTouch">
                         <h1 className="yeseva-header">Get your design right, right now</h1>
-                        <p className="my-3">Be the first know our latest offers and updates!</p>
+                        <p className="my-4">Be the first know our latest offers and updates!</p>
                         <div className="signIn-box d-flex justify-content-between">
                             <input className="bg-transparent border-0" placeholder="Enter your email addresss" type="email" />
                             <button type="submit">Get Started</button>
